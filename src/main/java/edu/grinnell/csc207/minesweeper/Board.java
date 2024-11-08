410f84d0538f870ca2d86274f7547120da740625
cyphersa <cyphersa@grinnell.edu>
2024-11-07 18:36:31 -0600

    Documentation for board

--- src/main/java/edu/grinnell/csc207/minesweeper/Board.java
+++ src/main/java/edu/grinnell/csc207/minesweeper/Board.java
@@ -15,69 +15,107 @@ import java.util.List;
 import java.util.Random;
 
 
-public class Board{
+/* 
+ * Creates the board of integers for the minesweeper game.
+*/
+public class Board {
     
+  // +--------+------------------------------------------------------
+  // | Fields |
+  // +--------+
+
+  /**
+   * The matrix of integers.
+   */
   private Matrix <Integer> matrix;
 
   
-  public void initializeBoard(int w, int h){
-    matrix = new MatrixV0<Integer>(h,w,Integer.valueOf(0));
-  }
+  // +--------------+------------------------------------------------
+  // | Constructors |
+  // +--------------+
 
-  public int mineCounter(int r, int c){
-    int count = 0;
+
+    /**
+   * Creates the minesweeper grid where each cell contains either the amount of mines
+   * surrounding it or -1 if the cell contains a mine.
+   * 
+   * @param w The width of the minesweeper board.
+   * @param h The height of the minesweeper board.
+   * @param mines The number of mines in the minesweeper board.
+   */
+  public Board(int w, int h, int mines) {
     
-    if(matrix.get(r,c).intValue()==-1){
-      return -1;
-    }
-
-    for(int i = r-1; i<=r+1; i++){
-      for(int j = c-1; j<=c+1; j++){
-        if(!(i<0 || j<0 || i>=matrix.height() || j>=matrix.width())){
-          if(matrix.get(i,j).intValue()==-1){
-            count++;
-          }
-        }
-      }
-    }
-
-    return count;
-  }
-
-
-  public Matrix <Integer> getMatrix(){
-    return this.matrix;
-  }
-
-
-  public Board(int w, int h, int mines){
-
-    initializeBoard(h, w);
-        
-    while(mines != 0){
+    //Initializes the each element to 0.
+    matrix = new MatrixV0 <Integer>(w, h, Integer.valueOf(0));
+    
+    //Inserts mines into the minesweeper board
+    while(mines != 0) {
       int r = (int)(Math.random() * h);
       int c = (int)(Math.random() * w);
-      System.out.printf("%d:%d\n", r,c);
 
-      if(matrix.get(r,c).intValue()!=-1){
+      if(matrix.get(r, c).intValue() != -1) {
         mines--;
-        System.out.printf("%d\n", mines);
-        matrix.set(r,c,Integer.valueOf(-1));
-        
-      }
-    }
-    System.out.printf("Made it out\n");
+        matrix.set(r, c, Integer.valueOf(-1));
+      } // if
+    } // while-loop
+ 
+    //Inserts the numbers counting the surrounding mines into the minesweeper board.
+    for(int i = 0; i < h; i++) {
+      for(int j = 0; j < w; j++) {
+        matrix.set(i, j, Integer.valueOf(mineCounter(i,j)));
+      } // for-loop
+    } // for-loop
+  } // Board(int, int, int)
 
-    for(int i = 0; i<h; i++){
-      for(int j = 0; j<w; j++){
-        matrix.set(i,j, Integer.valueOf(mineCounter(i,j)));
-      }
-    }
-  }
+
+  // +--------------+------------------------------------------------
+  // | Core methods |
+  // +--------------+
+
+
+    /**
+   * Gets the given matrix.
+   * 
+   * @return the integer matrix.
+   */
+  public Matrix <Integer> getMatrix() {
+    return this.matrix;
+  } // getMatrix()
   
 
+  /**
+   * Counts the number of mines surrounding the given cell. Returns -1 if the cell is a mine.
+   * 
+   * @param r The row of the cell.
+   * @param c The column of the cell.
+   * 
+   * @return An integer representing the number of mines around the cell or -1 if there is a mine.
+   */
+  public int mineCounter(int r, int c) {
+    int count = 0;
+    
+    //Checks to see if the given cell contains a mine.
+    if(matrix.get(r, c).intValue() == -1) {
+      return -1;
+    } // if
+
+    //Counts the number of mines surrounding the cell.
+    for(int i = r-1; i <= r+1; i++) {
+      for(int j = c-1; j <= c+1; j++){
+        if(!(i < 0 || j < 0 || i >= matrix.height() || j >= matrix.width())) {
+          if(matrix.get(i,j).intValue() == -1) {
+            count++;
+          } // if
+        } // if
+      } // for-loop
+    } // for-loop
+
+    return count;
+  } // mineCounter(int, int)
+
   
-  public static void main(String[] args){
+
+  public static void main(String[] args) {
     PrintWriter pen = new PrintWriter(System.out, true);
     Board minesweeper = new Board(14,18,40);
     Matrix.print(pen,minesweeper.getMatrix());
@@ -86,4 +124,4 @@ public class Board{
     display.print();
   }
   
-}
+} // class Board
